03e29aa28623aac07c153fc9222f1ac21069cbee
lakagar <lakagar2024@gmail.com>
2025-04-19 14:58:13 +0530

    Create wave-pattern.tsx

---
+++ wave-pattern.tsx
@@ -0,0 +1,32 @@
+<svg width="1200" height="200" xmlns="http://www.w3.org/2000/svg">
+  <defs>
+    <linearGradient id="waveGradient" x1="0%" y1="0%" x2="100%" y2="0%">
+      <stop offset="0%" style="stop-color:#E1F5FE;stop-opacity:0.7" />
+      <stop offset="50%" style="stop-color:#B3E5FC;stop-opacity:0.5" />
+      <stop offset="100%" style="stop-color:#81D4FA;stop-opacity:0.7" />
+    </linearGradient>
+  </defs>
+  <path d="M0,50 
+           C200,100 400,0 600,50 
+           C800,100 1000,0 1200,50 
+           L1200,200 
+           L0,200 
+           Z" 
+        fill="url(#waveGradient)" />
+  <path d="M0,80 
+           C200,130 400,30 600,80 
+           C800,130 1000,30 1200,80 
+           L1200,200 
+           L0,200 
+           Z" 
+        fill="url(#waveGradient)" 
+        opacity="0.6" />
+  <path d="M0,110 
+           C200,160 400,60 600,110 
+           C800,160 1000,60 1200,110 
+           L1200,200 
+           L0,200 
+           Z" 
+        fill="url(#waveGradient)" 
+        opacity="0.4" />
+</svg>
